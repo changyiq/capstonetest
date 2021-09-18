c80a46bcaf7e88364f17288b39b1b4b91892b38f
AvaXiaohongDeng <xiaohongdeng196@gmail.com>
2021-09-18 13:08:14 -0400

    Add taking selfie function

--- app/src/main/java/project/capstone6/acne_diagnosis/TakeSelfie.kt
+++ app/src/main/java/project/capstone6/acne_diagnosis/TakeSelfie.kt
@@ -1,17 +1,31 @@
 package project.capstone6.acne_diagnosis
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.FileProvider
 import project.capstone6.acne_diagnosis.databinding.ActivityTakeSelfieBinding
+import java.io.File
+
+private const val FILE_NAME ="selfie.jpg"
+private const val REQUEST_CODE =42
+private lateinit var photoFile: File
 
 class TakeSelfie : AppCompatActivity() {
 
     private lateinit var binding2: ActivityTakeSelfieBinding
     private lateinit var btnTakeSelfie : Button
     private lateinit var btnDiagnosis : Button
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +35,46 @@ class TakeSelfie : AppCompatActivity() {
 
         btnTakeSelfie = binding2.btnTakeSelfie
         btnDiagnosis = binding2.btnDiagnosis
+        imageView = binding2.imageView
+
+        //click the button to launch the intent to open the camera
+        btnTakeSelfie.setOnClickListener(){
+            val takeSelfieIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+           photoFile = getPhotoFile(FILE_NAME)
+
+
+           val fileProvider = FileProvider.getUriForFile(this,"project.capstone6.fileprovider", photoFile)
+           takeSelfieIntent.putExtra(MediaStore.EXTRA_OUTPUT,fileProvider)
+
+            if(takeSelfieIntent.resolveActivity(this.packageManager) != null){
+                startActivityForResult(takeSelfieIntent, REQUEST_CODE)
+            }else{
+                Toast.makeText(this,"Unable to open camera",Toast.LENGTH_LONG).show()
+            }
+        }
 
         btnDiagnosis.setOnClickListener {
 
             val intent2 = Intent(this, Result::class.java)
             startActivity(intent2)
+        }
+    }
+
+    //to access package in specific directories
+    private fun getPhotoFile(fileName:String):File{
+        val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName,".jpg",storageDirectory)
+    }
+
+
+
+    //Take selfie and upload it to ImageView
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+            imageView.setImageBitmap(takenImage)
+        } else{
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
