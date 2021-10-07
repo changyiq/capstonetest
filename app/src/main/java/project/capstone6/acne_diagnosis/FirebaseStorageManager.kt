a78ace009d2b09e37b45456b937cb59e9a727a6b
AvaXiaohongDeng <xiaohongdeng196@gmail.com>
2021-10-07 17:54:50 -0400

    test to add subdirectory for each selfie.

--- app/src/main/java/project/capstone6/acne_diagnosis/FirebaseStorageManager.kt
+++ app/src/main/java/project/capstone6/acne_diagnosis/FirebaseStorageManager.kt
@@ -4,6 +4,8 @@ import android.app.ProgressDialog
 import android.content.Context
 import android.net.Uri
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.storage.FirebaseStorage
 import java.util.*
 
@@ -11,6 +13,8 @@ class FirebaseStorageManager {
 
     private val mStorageRef = FirebaseStorage.getInstance().reference
     private lateinit var mProgressDialog: ProgressDialog
+    private var user: FirebaseUser? = null
+    private var subDir: String =""
 
     //upload image into firebase cloud.
     fun uploadImage(context: Context, imageFileUri: Uri) {
@@ -18,11 +22,16 @@ class FirebaseStorageManager {
         mProgressDialog.setMessage("Please wait, image being upload")
         mProgressDialog.show()
         val date = Date()
-        val uploadTask = mStorageRef.child("Test1/Pictures/${date}.png").putFile(imageFileUri)
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        subDir = user?.uid + "/${user?.uid} - ${date}/"
+
+        //val uploadTask = mStorageRef.child("Test1/Pictures/${date}.png").putFile(imageFileUri)
+        val uploadTask = mStorageRef.child(subDir+"Selifie.png").putFile(imageFileUri)
         uploadTask.addOnSuccessListener {
             Log.e("Frebase", "Image Upload success")
             mProgressDialog.dismiss()
-            val uploadedURL = mStorageRef.child("Test1/Pictures/${date}.png").downloadUrl
+            //val uploadedURL = mStorageRef.child("Test1/Pictures/${date}.png").downloadUrl
+            val uploadedURL = mStorageRef.child(subDir+"Selifie.png").downloadUrl
             Log.e("Firebase", "Uploaded $uploadedURL")
         }.addOnFailureListener {
             Log.e("Frebase", "Image Upload fail")
