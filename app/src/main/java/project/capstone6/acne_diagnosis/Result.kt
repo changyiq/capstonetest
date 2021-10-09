a97261e015bb5a70f20ce10a5086b55cce97a85b
AvaXiaohongDeng <xiaohongdeng196@gmail.com>
2021-10-09 10:31:24 -0400

    pass subdirectory to Result page

--- app/src/main/java/project/capstone6/acne_diagnosis/Result.kt
+++ app/src/main/java/project/capstone6/acne_diagnosis/Result.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import project.capstone6.acne_diagnosis.databinding.ActivityResultBinding
 
 class Result : AppCompatActivity() {
@@ -12,6 +15,7 @@ class Result : AppCompatActivity() {
     private lateinit var binding3:ActivityResultBinding
     private lateinit var btnAgain:Button
     private lateinit var btnExit:Button
+    private lateinit var tvResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,13 @@ class Result : AppCompatActivity() {
 
         btnAgain = binding3.btnAgain
         btnExit = binding3.btnExit
+        tvResult = binding3.tvResult
+
+        //Get subdirectory
+        val intent = getIntent()
+        val subDir= intent.getStringArrayExtra(TakeSelfie.EXTRA_SUBDIRECTORY)
+        Toast.makeText(this,"Subdir is " + subDir, Toast.LENGTH_LONG).show()
+        tvResult.text = "Diagnosing..."
 
         btnAgain.setOnClickListener {
 
