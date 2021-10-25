090ebc32993d8722cba105f551ac4ca705cdff6a
Tang-Y <miawu777@gmail.com>
2021-10-25 15:19:40 -0400

    Add and improve Result.kt UI, add symptom and medical resources enum for checking respond, create database and save users into database

---
+++ app/src/main/java/project/capstone6/acne_diagnosis/MedicalResourcesEnum.kt
@@ -0,0 +1,29 @@
+package project.capstone6.acne_diagnosis
+
+enum class MedicalResourcesEnum (val website: String) {
+    AR1("https://www.healthlinkbc.ca/health-topics/hw199515"),
+    AR2("https://www.healthlinkbc.ca/health-topics/tr5970"),
+    AM("https://www.aad.org/public/diseases/skin-cancer/actinic-keratosis-overview"),
+    AD("https://www.healthlinkbc.ca/health-topics/hw216104"),
+    BD("https://my.clevelandclinic.org/health/diseases/15855-bullous-pemphigoid"),
+    CI("https://www.mayoclinic.org/diseases-conditions/impetigo/symptoms-causes/syc-20352352"),
+    EC("https://www.aboutkidshealth.ca/Article?contentid=773&language=English&utm_source=google-grant&utm_medium=cpc&utm_campaign=AKH%20Generic&utm_term=Dermatology%20Eczema&gclid=CjwKCAjw_L6LBhBbEiwA4c46uoPCu3WAajjGylKsgVTE-EVJuRuI89V00KamOljJCxn7kDBB945BjxoCG_MQAvD_BwE&gclsrc=aw.ds"),
+    EDE("https://dermnetnz.org/topics/exanthems"),
+    HAIR("https://www.aad.org/public/diseases/hair-loss/types/alopecia"),
+    HPV("https://www.medicalnewstoday.com/articles/322674"),
+    PI("https://my.clevelandclinic.org/health/articles/11014-pigmentation-abnormal-pigmentation"),
+    CTD("https://lupuscanada.org/living-with-lupus/lupus-qa-ask-the-experts/?gclid=CjwKCAjw_L6LBhBbEiwA4c46uow7UoIWAW9dfPbRsQcf5E7hNWUYyubELaAvo_e3IUp1op_h4Ek-JRoCPf8QAvD_BwE"),
+    MM1("https://www.cancer.org/cancer/melanoma-skin-cancer/about/what-is-melanoma.html"),
+    MM2("https://my.clevelandclinic.org/health/diseases/4410-moles"),
+    NAIL("https://www.healthlinkbc.ca/health-topics/hw268101"),
+    CD("https://www.merckmanuals.com/en-ca/professional/dermatologic-disorders/dermatitis/contact-dermatitis"),
+    PSO("https://www.healthlinkbc.ca/health-topics/hw58469"),
+    SLD("http://www.bccdc.ca/health-info/diseases-conditions/scabies"),
+    SK("https://www.healthlinkbc.ca/health-topics/tn8432"),
+    SD("https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4753361/"),
+    TRC("https://www.cdc.gov/fungal/diseases/ringworm/definition.html"),
+    UH("https://www.worldallergy.org/education-and-programs/education/allergic-disease-resource-center/professionals/urticaria"),
+    VT("https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4078200/"),
+    VP("https://www.healthlinkbc.ca/health-topics/abq3686"),
+    WM("https://www.healthlinkbc.ca/health-topics/aa18143")
+}
---
+++ app/src/main/java/project/capstone6/acne_diagnosis/SymptomEnum.kt
@@ -0,0 +1,27 @@
+package project.capstone6.acne_diagnosis
+
+enum class SymptomEnum (val symptom: String) {
+    AR("Acne and Rosacea"),
+    AM("Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions"),
+    AD("Atopic Dermatitis"),
+    BD("Bullous Disease"),
+    CI("Cellulitis Impetigo and other Bacterial Infections"),
+    EC("Eczema"),
+    EDE("Exanthems and Drug Eruptions"),
+    HAIR("Hair Loss Photos Alopecia and other Hair Diseases"),
+    HPV("Herpes HPV and other STDs"),
+    PI("Light Diseases and Disorders of Pigmentation"),
+    CTD("Lupus and other Connective Tissue diseases"),
+    MM("Melanoma Skin Cancer Nevi and Moles"),
+    NAIL("Nail Fungus and other Nail Disease"),
+    CD("Contact Dermatitis"),
+    PSO("Psoriasis"),
+    SLD("Scabies Lyme Disease"),
+    SK("Seborrheic Keratoses"),
+    SD("Systemic Disease"),
+    TRC("Tinea Ringworm Candidiasis"),
+    UH("Urticaria Hives"),
+    VT("Vascular Tumors"),
+    VP("Vasculitis"),
+    WM("Warts Molluscum")
+}
