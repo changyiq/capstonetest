a1f8f2aa80e7c1833cb676e1d30e3993dab4907f
Tang-Y <miawu777@gmail.com>
2021-10-26 16:24:13 -0400

    Reduce redundant

--- app/src/main/java/project/capstone6/acne_diagnosis/TakeSelfie.kt
+++ app/src/main/java/project/capstone6/acne_diagnosis/TakeSelfie.kt
@@ -1,65 +1,55 @@
 package project.capstone6.acne_diagnosis
 
 import android.app.Activity
-import android.app.ProgressDialog
-import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.media.Image
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.os.Environment.getExternalStoragePublicDirectory
 import android.provider.MediaStore
-import android.util.Log
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
-import com.android.volley.*
+import com.android.volley.AuthFailureError
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.facebook.login.LoginManager
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
-import org.json.JSONException
 import org.json.JSONObject
 import project.capstone6.acne_diagnosis.databinding.ActivityTakeSelfieBinding
+import java.io.ByteArrayOutputStream
 import java.io.File
-import java.nio.charset.Charset
 import java.util.*
 
-private const val FILE_NAME ="selfie"
+
+private const val FILE_NAME = "selfie"
+
 class TakeSelfie : AppCompatActivity() {
 
-    val TAG ="TakeSelfie"
-
+    //val TAG = "TakeSelfie"
     private lateinit var binding2: ActivityTakeSelfieBinding
-    private lateinit var btnTakeSelfie : Button
-    private lateinit var btnDiagnosis : Button
+    private lateinit var btnTakeSelfie: Button
+    private lateinit var btnDiagnosis: Button
     private lateinit var imageView: ImageView
-    private lateinit var ImageUri: Uri
     private lateinit var photoFile: File
     private lateinit var fileProvider: Uri
+    private lateinit var takenImage: Bitmap
 
     private lateinit var subDir: String
     private lateinit var fullDir: String
 
-    var volleyRequestQueue: RequestQueue? = null
-    //val url: String = "http://localhost:49623/"
-    val url: String = "https://reqres.in/api/dir"
-
-    var dialog: ProgressDialog? = null
 
     var firebaseAuth: FirebaseAuth? = null
 
@@ -79,124 +69,111 @@ class TakeSelfie : AppCompatActivity() {
         btnDiagnosis = binding2.btnDiagnosis
         imageView = binding2.imageView
 
-
-
         // Initialise Firebase
         firebaseAuth = FirebaseAuth.getInstance()
-        val currentUser = firebaseAuth!!.currentUser
-
-
-        //clear subDir
-        subDir =""
+        //val currentUser = firebaseAuth!!.currentUser
 
         //click the button to invoke an intent to take a selfie
-        btnTakeSelfie.setOnClickListener(){
+        btnTakeSelfie.setOnClickListener() {
             val takeSelfieIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             photoFile = getPhotoFile(FILE_NAME)
 
-            fileProvider = FileProvider.getUriForFile(this,"project.capstone6.fileprovider", photoFile)
-            takeSelfieIntent.putExtra(MediaStore.EXTRA_OUTPUT,fileProvider)
+            fileProvider =
+                FileProvider.getUriForFile(this, "project.capstone6.fileprovider", photoFile)
+            takeSelfieIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
 
-            if(takeSelfieIntent.resolveActivity(this.packageManager) != null){
+            if (takeSelfieIntent.resolveActivity(this.packageManager) != null) {
                 startActivityForResult(takeSelfieIntent, REQUEST_FROM_CAMERA)
-            }else{
-                Toast.makeText(this,"Unable to open camera",Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Unable to open camera", Toast.LENGTH_LONG).show()
             }
         }
         btnDiagnosis.setOnClickListener {
 
-            if(subDir != "" && subDir != null){
+            if (subDir != "" && subDir != null) {
                 // call intent to go to result page
                 val intent = Intent(this, Result::class.java)
 
                 //pass fulldirectory information to Result page
                 //Toast.makeText(this," Save Subdir as " + subDir,Toast.LENGTH_LONG).show()
-                intent.putExtra(EXTRA_FULLDIRECTORY,fullDir)
-                intent.putExtra(EXTRA_SUBDIRECTORY,subDir)
+                intent.putExtra(EXTRA_FULLDIRECTORY, fullDir)
+                intent.putExtra(EXTRA_SUBDIRECTORY, subDir)
                 startActivity(intent)
 
-                //pass directory to API
-                postVolley(fullDir,subDir)
+                //upload image to API by Volley
+                postImageByVolley(takenImage)
 
-            }else {
+                //upload image to API by HttpURLConnection
+                //postImageByHttpURLConnection(takenImage)
 
-                // Tell user to wait
-                Toast.makeText(this,"Please take selfie for skin, or check for your history analysis.",Toast.LENGTH_LONG).show()
+                //pass path message to API
+                //postPathByVolley(fullDir)
+
+                //clear subDir
+                subDir = ""
             }
         }
     }
 
     //to create a file for the selfie
-    private fun getPhotoFile(fileName:String):File{
+    private fun getPhotoFile(fileName: String): File {
         val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(fileName,".jpg",storageDirectory)
+        return File.createTempFile(fileName, ".jpg", storageDirectory)
     }
 
     //to Retrieve the selfie， display it in an ImageView and upload into Firebase cloud
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode == REQUEST_FROM_CAMERA && resultCode == Activity.RESULT_OK){
+        if (requestCode == REQUEST_FROM_CAMERA && resultCode == Activity.RESULT_OK) {
 
-            val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+            //getting image from the file stored the selfie
+            takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
             imageView.setImageBitmap(takenImage)
-            Toast.makeText(this, "image saved", Toast.LENGTH_SHORT).show()
 
             //uploadImage(this, fileProvider)
             subDir = FirebaseStorageManager().uploadImage(this, fileProvider)
             fullDir = "gs://acne-diagnosis-6a653.appspot.com/" + subDir
-
-            // add image into realtime database
-            // Write a message to the database
-            val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("Users")
-
-            val user = FirebaseAuth.getInstance().currentUser
-            val uid = user?.uid
-
-            // Get column from the table
-            // get the user nd add the data
-            myRef.child(uid.toString()).get().addOnSuccessListener {
-                if (it.child("image").exists()) {
-                    myRef.child(uid.toString()).child("image").setValue(fullDir)
-                }else if (!it.child("image").exists()){
-                    myRef.child(uid.toString()).child("image").setValue(fullDir)
-                    //myRef.child(uid.toString()).child("result").setValue(SymptomEnum.AD)
-                }
-            }
-        } else{
+            Toast.makeText(this, "Selfie upload to Firebase", Toast.LENGTH_SHORT).show()
+        } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
-    fun postVolley(fullDirectory: String, subDirectory: String) {
+    fun postPathByVolley(fullDirectory: String) {
 
-        // Post parameters
-        // Form fields and values
-        val params = HashMap<String,String>()
+        //val url1: String = "http://localhost:44374/api/Image"
+        val url1: String = "https://reqres.in/api/dir"
+
+        // Post parameters, Form fields and values
+        val params = HashMap<String, String>()
         params["fullDirectory"] = fullDirectory
-        params["subDirectory"] = subDirectory
         val jsonObject = JSONObject(params as Map<*, *>)
 
         // Volley post request with parameters
-        val request = JsonObjectRequest(
-            Request.Method.POST,url,jsonObject,
+        val request1 = JsonObjectRequest(
+            Request.Method.POST, url1, jsonObject,
             Response.Listener { response ->
                 // Process the json
                 try {
                     //textView.text = "Response: $response"
-                    Toast.makeText(this, "Response: \nMessage be posted to API. \n$response", Toast.LENGTH_LONG).show()
-                }catch (e:Exception){
+                    Toast.makeText(
+                        this,
+                        "Response: \nPath are posted to API. \n$response",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } catch (e: Exception) {
                     //textView.text = "Exception: $e"
                     Toast.makeText(this, "Exception: $e", Toast.LENGTH_LONG).show()
                 }
 
-            }, Response.ErrorListener{
+            }, Response.ErrorListener {
                 // Error in request
                 //textView.text = "Volley error: $it"
                 Toast.makeText(this, "Volley error: $it", Toast.LENGTH_LONG).show()
             })
 
+
         // Volley request policy, only one time request to avoid duplicate transaction
-        request.retryPolicy = DefaultRetryPolicy(
+        request1.retryPolicy = DefaultRetryPolicy(
             DefaultRetryPolicy.DEFAULT_TIMEOUT_MS,
             // 0 means no retry
             0, // DefaultRetryPolicy.DEFAULT_MAX_RETRIES = 2
@@ -204,8 +181,76 @@ class TakeSelfie : AppCompatActivity() {
         )
 
         // Add the volley post request to the request queue
-        VolleySingleton.getInstance(this).addToRequestQueue(request)
+        VolleySingleton.getInstance(this).addToRequestQueue(request1)
 
+    }
+
+    /*
+    fun postImageByHttpURLConnection(bitmap: Bitmap) {
+        try {
+            val url = URL("http://localhost:44374/api/Image")
+            val c: HttpURLConnection = url.openConnection() as HttpURLConnection
+            c.setDoInput(true)
+            c.setRequestMethod("POST")
+            c.setDoOutput(true)
+            c.connect()
+
+            val output: OutputStream = c.getOutputStream()
+            bitmap.compress(CompressFormat.JPEG, 50, output)
+            output.close()
+
+            val result = Scanner(c.getInputStream())
+            val response: String = result.nextLine()
+            Log.e("ImageUploader", "Error uploading image: $response")
+
+            result.close()
+        } catch (e: IOException) {
+            Log.e("ImageUploader", "Error uploading image", e)
+        }
+    }
+    */
+
+
+    fun postImageByVolley(image: Bitmap) {
+
+        val url2: String = "http://localhost:44374/api/Image"
+
+        //converting image to base64 string
+        val baos = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val imageBytes = baos.toByteArray()
+        val imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT)
+
+        //sending image to server
+        val request2: StringRequest = object : StringRequest(
+            Method.POST, url2,
+            Response.Listener { s ->
+                if (s == "true") {
+                    Toast.makeText(this@TakeSelfie, "Uploaded Image Successful", Toast.LENGTH_LONG)
+                        .show()
+                } else {
+                    Toast.makeText(this@TakeSelfie, "Some error occurred!", Toast.LENGTH_LONG)
+                        .show()
+                }
+            },
+            Response.ErrorListener { volleyError ->
+                Toast.makeText(
+                    this@TakeSelfie,
+                    "Some error occurred -> $volleyError",
+                    Toast.LENGTH_LONG
+                ).show()
+            }) {
+            //adding parameters to send
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String>? {
+                val parameters: MutableMap<String, String> = HashMap()
+                parameters["image"] = imageString
+                return parameters
+            }
+        }
+
+        // Add the volley post request to the request queue
+        VolleySingleton.getInstance(this).addToRequestQueue(request2)
     }
 
     //process menu
@@ -257,7 +302,7 @@ class TakeSelfie : AppCompatActivity() {
         finish()
 
         // Facebook login status check
-        val currentUser = firebaseAuth!!.currentUser
+        //val currentUser = firebaseAuth!!.currentUser
         firebaseAuth!!.signOut()
         LoginManager.getInstance().logOut()
         finish()
